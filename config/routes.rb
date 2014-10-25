a3fa570a73bb364ea63ce708409d7ece7c7c2f4c
Mark Brewster <m.brewster@volcanic.co.uk>
2014-10-25 15:08:22 +0100

    tranis view created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :messages
+  
+   get 'trains' => 'trains#index'
+   
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
